21e9048a060dbddb3f4c01a03b5737d52b59964b
Joey C <joeycasabar@gmail.com>
2020-06-07 19:51:14 -0400

    fixed numbering issue and missing semicolon

--- vhd/tutorial1.vhd
+++ vhd/tutorial1.vhd
@@ -9,12 +9,12 @@ entity Switches_To_LEDs is
   i_Switch_1 : in std_logic;
   i_Switch_2 : in std_logic;
   i_Switch_3 : in std_logic;
-  i_Switch_4 : in std_logic
+  i_Switch_4 : in std_logic;
 
   o_LED_1 : out std_logic;
-  o_LED_1 : out std_logic;
-  o_LED_1 : out std_logic;
-  o_LED_1 : out std_logic
+  o_LED_2 : out std_logic;
+  o_LED_3 : out std_logic;
+  o_LED_4 : out std_logic
   );
 
 end entity Switches_To_LEDs;
